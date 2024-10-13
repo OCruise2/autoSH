39f3e5911b34055793a8854e6d739baafa5f7586
Odhran Cruise <odhran.cruise@chem.ox.ac.uk>
2024-10-13 14:04:38 +0100

    Copied qorca, filepaths are incorrect, needs more commenting

--- orcaSH.py
+++ orcaSH.py
@@ -2,7 +2,32 @@ import os
 import argparse
 
 def get_args():
-    return
+    '''Get the command line arguements passed to this script using argparse'''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", action='store',
+                        help='.inp file(s) submit to the queue',
+                        nargs='+')
+    
+    parser.add_argument("-ca", "--copy_all",
+                        action='store_true',
+                        default=False,
+                        help='Copy all of the files in the current directory'
+                        'to the compute node.')
+    
+    parser.add_argument('-cs', "--copy_scratch",
+                        action='store_true',
+                        default=False,
+                        help='Copy all files from the scratch directory back'
+                        'to this directory when the calculation is '
+                        'finished.')
+    
+    parser.add_argument('-np', '--num_processors',
+                        type=int,
+                        default=0,
+                        help="Override the number of cores specified in the "
+                        "input file.")
+    return parser.parse_args()
 
 def num_cores(inp_filename, args):
     '''Gets the number of cores to request from the .inp file
@@ -42,6 +67,55 @@ def num_cores(inp_filename, args):
 
     return _num_cores
 
-def make_sh_file():
-    return
+def make_sh_file(sh_filename, inp_filename, args):
+    """
+    Print the submission script appropriate for an ORCA input file.
+    ---------------------------------------------------------------
+    Arguments:
+        sh_filename (str): Submission script filename
+        inp_filename (str): Input filename
+        args (Namespace): Command line arguents
+    """
+    with open(sh_filename, 'w') as sh_file:
+        print('#!/bin/bash',
+              '#$ -cwd',
+              f'#$ -pe smp {num_cores(inp_filename, args)}',
+              '#$ -l s_rt=48:00:00',
+              '#',
+              'export ORIG=$PWD',
+              'export SCR=$TMPDIR',
+              'export NBOEXE=/usr/local/nbo7/bin/nbo7.i4.exe',
+              f'module load {mpi_version}',
+              f'cp {"*" if args.copy_all else inp_filename} $SCR',
+              f'cd $SCR',
+              f'{orca_path} {inp_filename} > {inp_filename.replace(".inp", ".out")}',
+              'rm -f *.tmp',
+              sep ='/n', file=sh_file)
+        
+        if args.copy_scratcj:
+            print('cp -R * $ORIG', file=sh_file)
+        else:
+            print('cp *.xyz *.hess *.out $ORIG', file=sh_file)
 
+        print('rm *.sh.*', file=sh_file)
+
+    return None
+
+if __name__ == '__main__':
+    arguments=get_args()
+
+    for filename in arguments.filenames:
+        if not filename.endswith('.inp'):
+            exit(f"Filename must end with .inp. Found: {filename}")
+
+        script_filename = filename.replace('.inp', '.sh')
+
+        #Cannot start script names with a digit - will add '_' in front
+        if script_filename[0].isdigit():
+            script_filename = f'_{script_filename}'
+
+        make_sh_file(script_filename,
+                     inp_filename=filename,
+                     args=arguments)
+        
+        os.system(f'qsub {script_filename}')
