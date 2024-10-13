513b685fd67e3d988ef42c78056b91842c9ded93
Odhran Cruise <odhran.cruise@chem.ox.ac.uk>
2024-10-13 15:06:50 +0100

    Added -mem to args, rewrote num_cores to only go through .inp once instead of twice, rewrote make_sh_file to include correct filepaths and syntax

--- orcaSH.py
+++ orcaSH.py
@@ -2,7 +2,8 @@ import os
 import argparse
 
 def get_args():
-    '''Get the command line arguements passed to this script using argparse'''
+    '''Get the command line arguments passed to this script using argparse. 
+    Nothing aside from the .inp filenames is critical'''
 
     parser = argparse.ArgumentParser()
     parser.add_argument("filenames", action='store',
@@ -13,20 +14,25 @@ def get_args():
                         action='store_true',
                         default=False,
                         help='Copy all of the files in the current directory'
-                        'to the compute node.')
-    
+                        'to the compute node. Default is false.')
+
     parser.add_argument('-cs', "--copy_scratch",
                         action='store_true',
                         default=False,
                         help='Copy all files from the scratch directory back'
                         'to this directory when the calculation is '
-                        'finished.')
+                        'finished. Default is false.')
     
     parser.add_argument('-np', '--num_processors',
                         type=int,
                         default=0,
                         help="Override the number of cores specified in the "
-                        "input file.")
+                        "input file. Defaults to zero, ")
+    
+    parser.add_argument('-mem', '--set_memory',
+                        type=int,
+                        default=3,
+                        help='Sets the memory per core of the calculation. Defaults to 3Gb per core')
     return parser.parse_args()
 
 def num_cores(inp_filename, args):
@@ -36,32 +42,36 @@ def num_cores(inp_filename, args):
         (int): Number of cores
 
     Raises:
-        (ValueError, IndexError): If input file contains error
+        ValueError: If the input file contains an error or is not correctly formatted.
     '''
-    _num_cores = 1 #Defaults to 1 if none specified
+    _num_cores = 1  # Defaults to 1 if none specified
 
     try:
-        keyword_line = next(line for line in open(inp_filename, 'r')
-                            if line.startswith('!'))
-        
-        #Can find number of cores line by starting with PAL in initial command line
-        for item in keyword_line.split():
-            if item.lower().startswith('pal'):
-                _num_cores = int(item[3:])
+        with open(inp_filename, 'r') as inp_file:
+            for line in inp_file:
+                # Check for '!' line for PAL
+                if line.startswith('!'):
+                    for item in line.split():
+                        if item.lower().startswith('pal'):
+                            _num_cores = int(item[3:])
+                            break
+                
+                # Check for '%pal' method for nprocs
+                if 'nprocs' in line.lower():
+                    parts = line.split()
+                    if 'nprocs' in parts:
+                        idx = parts.index('nprocs')
+                        if idx + 1 < len(parts):
+                            _num_cores = int(parts[idx + 1])
+                        else:
+                            raise ValueError(f"Invalid format for 'nprocs' in {inp_filename}")
 
-    except StopIteration:
-        exit(f'{inp_filename} was not correctly formatted. Must have a line started with a !')
-
-    #Could also use the %pal method in the input file (the correct way) - searching directly for nprocs
-    for line in open(inp_filename, 'r'):
-
-        if 'nprocs' in line:
-            idx = next(i for i, item in enumerate(line.split())
-                       if 'nprocs' == item.lower())
-            
-            _num_cores = int(line.split()[idx+1])
-
-    #If user specifies directly in command line input - will overwrite
+    except FileNotFoundError:
+        raise ValueError(f"Input file {inp_filename} not found.")
+    except ValueError as ve:
+        raise ValueError(f"Error parsing {inp_filename}: {ve}")
+    
+    # Override with command-line argument if provided
     if args.num_processors != 0:
         _num_cores = args.num_processors
 
@@ -70,35 +80,50 @@ def num_cores(inp_filename, args):
 def make_sh_file(sh_filename, inp_filename, args):
     """
     Print the submission script appropriate for an ORCA input file.
+    Currently uses ORCA v5.0.4
     ---------------------------------------------------------------
     Arguments:
         sh_filename (str): Submission script filename
         inp_filename (str): Input filename
         args (Namespace): Command line arguents
     """
+    orca_path = '/apps/applications/orca/5.0.4/1/default/bin/orca'
+
     with open(sh_filename, 'w') as sh_file:
-        print('#!/bin/bash',
-              '#$ -cwd',
-              f'#$ -pe smp {num_cores(inp_filename, args)}',
-              '#$ -l s_rt=48:00:00',
-              '#',
-              'export ORIG=$PWD',
-              'export SCR=$TMPDIR',
-              'export NBOEXE=/usr/local/nbo7/bin/nbo7.i4.exe',
-              f'module load {mpi_version}',
-              f'cp {"*" if args.copy_all else inp_filename} $SCR',
-              f'cd $SCR',
-              f'{orca_path} {inp_filename} > {inp_filename.replace(".inp", ".out")}',
-              'rm -f *.tmp',
-              sep ='/n', file=sh_file)
-        
-        if args.copy_scratcj:
-            print('cp -R * $ORIG', file=sh_file)
+        # #$/bin/bash specifies to run the .sh in bash instead of sh or zsh etc
+        # Submits for max 48 h runtime
+        sh_file.write(f"""#$/bin/bash
+#$ -V -cwd
+#$ -l h_rt=48:00:00
+#$ -pe smp {num_cores(inp_filename, args)}
+#$ -l h_vmem={args.set_memory}G
+#$ -m be
+
+export ORIG=$PWD
+export SCR=$TMPDIR #Provided by the system but not /scratch or /tmp. Assigned on a per job basis
+""")
+
+        #Copy files to the scratch
+        copy_files = '*' if args.copy_all else inp_filename
+        sh_file.write(f'cp {copy_files} $SCR\n')
+        sh_file.write("cd $SCR\n")
+
+        #ORCA command itself
+        output_filename = inp_filename.replace(".inp", ",out")
+        sh_file.write(f"{orca_path} {inp_filename} > {output_filename}\n")
+
+        #Cleaning up temp files - ORCA does this automatically, but if the calculation fails, 
+        # they will not be removed by default
+        sh_file.write("rm -f *.tmp\n")
+
+        #Copying results back to working directory. 
+        if args.copy_scratch:
+            sh_file.write("cp -R * $ORIG\n")
         else:
-            print('cp *.xyz *.hess *.out $ORIG', file=sh_file)
+            sh_file.write("cp *.xyz *.hess *.out $ORIG\n")
 
-        print('rm *.sh.*', file=sh_file)
-
+        #Removes shell output (comment out line for testing)
+        #sh_file.write("rm *.sh.*\n")
     return None
 
 if __name__ == '__main__':
