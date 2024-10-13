f423a760cfb2f4b4c8ad66551502b952b088e05f
Odhran Ubuntu <odhrancruise@gmail.com>
2024-10-13 16:53:46 +0100

    Fixed broken links

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-dl_orcaSH(){wget https://raw.github.com/OCruise2/autoSH/orcaSH.py 
+dl_orcaSH(){wget https://raw.githubusercontent.com/OCruise2/autoSH/main/orcaSH.py 
 -O ~/orcaSH
     chmod +x ~/orcaSH
     echo "export PATH\$HOME:\$PATH">>~/.bash_profile
