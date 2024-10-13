9710f8c5dfb98f845018f670810b16e35ab7e8c2
Odhran Ubuntu <odhrancruise@gmail.com>
2024-10-13 16:40:50 +0100

    Made install file

---
+++ install.sh
@@ -0,0 +1,8 @@
+dl_orcaSH(){wget https://raw.github.com/OCruise2/autoSH/orcaSH.py 
+-O ~/orcaSH
+    chmod +x ~/orcaSH
+    echo "export PATH\$HOME:\$PATH">>~/.bash_profile
+    source ~/.bash_profile
+    }
+
+dl_orcaSH
