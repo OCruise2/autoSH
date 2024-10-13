aae8e61031f50366ec1e7eba3acd97b17de3fa8a
Odhran Ubuntu <odhrancruise@gmail.com>
2024-10-13 16:56:41 +0100

    install.sh syntax

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-dl_orcaSH(){wget https://raw.githubusercontent.com/OCruise2/autoSH/main/orcaSH.py 
--O ~/orcaSH
+dl_orcaSH() {
+    wget https://raw.githubusercontent.com/OCruise2/autoSH/main/orcaSH.py -O ~/orcaSH
     chmod +x ~/orcaSH
-    echo "export PATH\$HOME:\$PATH">>~/.bash_profile
+    echo "export PATH=\"\$HOME:\$PATH\"" >> ~/.bash_profile
     source ~/.bash_profile
-    }
+}
 
 dl_orcaSH
