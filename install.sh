36ed275cae6a1bb546b841013937986b2b1e8807
Odhran Ubuntu <odhrancruise@gmail.com>
2024-10-13 17:10:43 +0100

    Updated install link for the bash version of orcaSH. Need HPC for testing

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 dl_orcaSH() {
-    wget https://raw.githubusercontent.com/OCruise2/autoSH/main/orcaSH.py -O ~/orcaSH
+    wget https://github.com/OCruise2/autoSH/blob/b9b9811e59f3995c0d4c61e33fd44f12fec5524b/bash_orcaSH.py -O ~/orcaSH
     chmod +x ~/orcaSH
     echo "export PATH=\"\$HOME:\$PATH\"" >> ~/.bash_profile
     source ~/.bash_profile
